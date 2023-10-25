be0c5595e5397985d004f9a86016237e78de3ec5
TrevorBelmont <tbelmont97@gmail.com>
2023-10-25 11:22:14 -0500

    Week4

--- Practicing Strings.js
+++ Practicing Strings.js
@@ -1,5 +1,14 @@
-let name = "Dana L. Wyatt"; 
+let namePerson = "Dana L. Wyatt"; 
 let favoriteQuote = 'God is good'; 
-let book = `Bible`;
+let favoriteBook = `Bible, `;
 
-let dialogue = 'God Said: "Let there be light"';
+let dialogueSpoken = 'God Said: "Let there be light"';
+let passageRead = "Genesis, ";
+
+let combinationPart1 = favoriteBook + favoriteQuote;
+let combinationPart2 = favoriteBook + namePerson;
+let combinationPart3 = passageRead + dialogueSpoken;
+
+console.log(combinationPart1);
+console.log(combinationPart2);
+console.log(combinationPart3);
