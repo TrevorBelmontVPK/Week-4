d5d614180649c0bbc063ec63912266c352f6b57b
TrevorBelmont <tbelmont97@gmail.com>
2023-10-25 11:45:09 -0500

    Week 4

---
+++ Practicing Strings 2.js
@@ -0,0 +1,12 @@
+let topicLearned = "JavaScript";
+let doingWhat = "I am learning ";
+
+let newTopic = "StringSplitting";
+let whatAgain = "A new thing I learned is "
+
+let topicToday2 = whatAgain.concat(newTopic);
+let topicToday = doingWhat + topicLearned;
+
+console.log(topicToday);
+console.log(topicToday2)
+
