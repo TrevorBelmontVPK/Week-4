b7ab324ce5f7fab5702775d5087bd67984dbd79f
TrevorBelmont <tbelmont97@gmail.com>
2023-10-26 13:51:22 -0500

    Week 4

---
+++ If Else exercise.js
@@ -0,0 +1,16 @@
+//If Else exercise
+
+const LIMIT = 250000
+let carMilage = 100000
+
+if(carMilage <= LIMIT){
+    console.log("All clear")
+}else{
+    console.log("NoGo")
+}
+
+if(carMilage >= 300000){
+    console.log("All clear")
+}else{
+    console.log("NoGo")
+}
