06e93f9406514032caa91093423bf437976837f8
TrevorBelmont <tbelmont97@gmail.com>
2023-10-27 14:16:21 -0500

    IceCream2023

---
+++ if else exercise 2.js
@@ -0,0 +1,23 @@
+//min_max.js
+
+let studentName = "Betty";
+let studentMajor = "HIST";
+let locationCourse
+let majorName 
+
+if(studentMajor == "ENG"){
+    locationCourse = "Kerr Hall, Room 201";
+    majorName = "English";
+}
+else if(studentMajor == "HIST"){
+    locationCourse = "Kerr Hall, Room 114";
+    majorName = "History";
+}else{
+    locationCourse = "nothing";
+    majorName = "unknown";
+}
+console.log(studentName)
+
+console.log(majorName)
+
+console.log(locationCourse)
